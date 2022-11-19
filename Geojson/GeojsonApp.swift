3fd8c0874a10069479ce73b32c3a79edd8ef748e
Jack Finnis <jack.finnis@icloud.com>
2022-11-19 16:08:24 +0000

    Initial Commit

---
+++ Geojson/GeojsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeojsonApp.swift
+//  Geojson
+//
+//  Created by Jack Finnis on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeojsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
